74f5ab9711a24f299e5c954115b08c80b70b23c2
TommyWang08 <woaifgg@163.com>
2024-09-02 05:50:55 +1000

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Test");
+    }
+}
+
