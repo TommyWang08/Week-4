9acddc176f6835ba86a00f168e49f514e653d8d6
TommyWang08 <woaifgg@163.com>
2024-09-02 06:46:55 +1000

    Renamed ConsolePrint to PrintAndHide, set Renderer references, and updated tags

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
+    [SerializeField] private Renderer rend;
     private int i = 3;
 
     void Update()
     {
         Debug.Log($"{gameObject.name}:{i}");
-
         i++;
     }
 }
+
 
